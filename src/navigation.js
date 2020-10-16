e7c92e9ef9507c2b2b02090ef63155d44933e370
Alaudae <hongyum19970506@gmail.com>
2020-10-16 18:04:37 -0400

    first commit

---
+++ src/navigation.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Menu } from 'semantic-ui-react'
+
+function NavigationBar() {
+
+    return (
+        <Menu pointing secondary>
+            <Menu.Item  name='home' href="/somelink"/>
+            <Menu.Item  name='posts' href="/somelink"/>
+            <Menu.Item  name='contact' href="/somelink"/>
+        </Menu>
+    )
+}
+
+export default NavigationBar;
