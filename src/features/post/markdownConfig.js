bfe68204862ae5d24a87a13c9e79aee21b67e9e0
Alaudae <hongyum19970506@gmail.com>
2020-11-18 17:07:07 -0500

    minor update

--- src/features/post/markdownConfig.js
+++ src/features/post/markdownConfig.js
@@ -1,7 +1,7 @@
 import React from "react";
 import gfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
-import { Table } from "semantic-ui-react";
+import { Image, Table } from "semantic-ui-react";
 
 function MarkdownTable(props) {
     return <Table celled>{props.children}</Table>;
@@ -15,20 +15,27 @@ function MarkdownTableBody(props) {
     return <Table.Body>{props.children}</Table.Body>;
 }
 
-function MarkdownTableRow(props){
+function MarkdownTableRow(props) {
     return <Table.Row>{props.children}</Table.Row>;
+}
+
+function MarkdownImage(props) {
+    return (
+        <Image src={props.src} style={{ maxWidth: "100%", height: "auto" }} />
+    );
 }
 
 export function Markdown({ children }) {
     return (
         <ReactMarkdown
-            plugins={[gfm]}
+            plugins={[gfm, { singleTilde: false }]}
             children={children}
             renderers={{
                 table: MarkdownTable,
                 tableHead: MarkdownTableHead,
                 tableBody: MarkdownTableBody,
                 tableRow: MarkdownTableRow,
+                image: MarkdownImage,
             }}
         />
     );
